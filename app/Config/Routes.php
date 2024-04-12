4e4a733879360876cfe0f8d123fd4b78ff3e5805
Carlos Perales <carlosperales050@gmail.com>
2024-04-11 21:49:35 -0600

    add: endpoint id

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,6 +13,7 @@ $routes->get('/eliminar/(:num)', 'Home::eliminar/$1');
 
 // ApiRest
 $routes->resource('restarticulos', ['controller' => "RestArticulos"]);
+$routes->get('restarticulos/(:num)', 'RestArticulos::show/$1');
 
 $routes->get('/index.php/mundo', 'Holamundo');
 $routes->get('/index.php/mundo/otra', 'Holamundo::desdeOtraCarpeta');
--- app/Controllers/RestArticulos.php
+++ app/Controllers/RestArticulos.php
@@ -10,7 +10,32 @@ class RestArticulos extends ResourceController
 
     public function index()
     {
-        return $this->respond($this->model->findAll());
+        return $this->genericResponse($this->model->findAll(),"",200);
     }
+    public function show($id=null)
+    {
+        $articulo= $this->model->find($id);
+        if($id==null){
 
+            return $this->genericResponse(null,"id not found",500);
+        }
+        if(!$articulo){
+            return $this->genericResponse(null,"id not found",500);
+        }
+        return $this->genericResponse($articulo,"",200);
+        
+    }
+    private function genericResponse($data,$messaje,$code){
+        if($code==200){
+            return $this->respond([
+                "data"=>$data,
+                "code"=>$code,
+            ]);
+        }else{
+            return $this-> respond([
+                "msj"=>$messaje,
+                "code"=>$code,
+            ]);
+        }
+    }
 }
