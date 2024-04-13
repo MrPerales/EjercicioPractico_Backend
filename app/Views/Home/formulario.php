0ec0965ae0b26cef188a86cc978557ba384090fd
Carlos Perales <carlosperales050@gmail.com>
2024-04-12 18:55:02 -0600

    add:styles formulario

--- app/Views/Home/formulario.php
+++ app/Views/Home/formulario.php
@@ -1,4 +1,4 @@
-<div>
+<div class='flex flex-row justify-center items-center gap-10 '>
    <!-- <label for="titulo">titulo</label>
    <label for="metatitulo">meta titulo</label> -->
 <?php
@@ -24,50 +24,75 @@
 
 
     <?php
-    echo form_open('/guarda');
+        echo form_open('/guarda');
 
-    echo form_label('Inserte Titulo', 'titulo');
-    echo form_input(array('name'=>'titulo',"placeholder"=>'titulo',"value"=>$titulo));
-    echo '<br>';
+        echo form_label('Inserte Titulo', 'titulo',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_input(array(
+            'name'=>'titulo',
+            "placeholder"=>'titulo',
+            "value"=>$titulo,
+            "class"=>"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/2 p-2.5",
+         ));
 
-    echo form_label('Inserte Meta Titulo', 'meta_titulo');
-    echo form_input(array('name'=>'meta_titulo',"placeholder"=>'Meta titulo',"value"=>$meta_titulo));
-    echo '<br>';
+        echo form_label('Inserte Meta Titulo', 'meta_titulo',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_input(array(
+            'name'=>'meta_titulo',
+            "placeholder"=>'Meta titulo',
+            "value"=>$meta_titulo,
+            "class"=>"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/2 p-2.5"
+         ));
 
-    echo form_label('Inserte Meta Descripcion', 'meta_descripcion');
-    echo form_textarea(array('name'=>'meta_descripcion',"placeholder"=>'Meta descripcion',"value"=>$meta_descripcion));
-    echo '<br>';
+        echo form_label('Inserte Meta Descripcion', 'meta_descripcion',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_textarea(array(
+            'name'=>'meta_descripcion',
+            "placeholder"=>'Meta descripcion',
+            "value"=>$meta_descripcion,
+            "class"=>"block p-2.5 w-72 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  "
 
-    echo form_label('Inserte Descripcion', 'descripcion');
-    echo form_textarea(array('name'=>'descripcion',"placeholder"=>'descripcion',"value"=>$descripcion));
-    echo '<br>';
+        ));
+
+        echo form_label('Inserte Descripcion', 'descripcion',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_textarea(array(
+            'name'=>'descripcion',
+            "placeholder"=>'descripcion',
+            "value"=>$descripcion ,
+            "class"=>"block p-2.5 w-72 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  "
+        ));
     
-    echo form_label('Inserte link de la Imagen', 'imagen');
-    echo form_input(array('name'=>'imagen',"placeholder"=>'imagen',"value"=>$imagen));
-    echo '<br>';
+    echo form_label('Inserte link de la Imagen', 'imagen',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_input(array(
+         'name'=>'imagen',
+          "placeholder"=>'imagen',
+          "value"=>$imagen,
+          "class"=>"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/2 p-2.5"
+        ));
 
-    echo form_label('Inserte contenido del articulo', 'contenido_del_articulo');
-    echo form_textarea(array('name'=>'contenido_del_articulo',"placeholder"=>'Contenido del articulo',"value"=>$contenido_del_articulo));
-    echo '<br>';
+    echo form_label('Inserte contenido del articulo', 'contenido_del_articulo',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_textarea(array(
+            'name'=>'contenido_del_articulo',
+            "placeholder"=>'Contenido del articulo',
+            "value"=>$contenido_del_articulo,
+            "class"=>"block p-2.5 w-72 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  "
+        ));
 
-    echo form_label('Inserte fecha de publicacion', 'fecha_de_programacion');
-    echo form_input(array('name'=>'fecha_de_programacion', 'type'=>"date","value"=>$fecha_de_programacion));
-    echo '<br>';
+        echo form_label('Inserte fecha de publicacion', 'fecha_de_programacion',["class"=>"block mb-2 text-sm font-medium text-gray-900 "]);
+        echo form_input(array(
+            'name'=>'fecha_de_programacion',
+            'type'=>"date",
+            "value"=>$fecha_de_programacion,
+        ));
 
-
-
-
-
-    echo form_submit('enviar','Enviar');
-    if(isset($articulos)){
-        echo form_hidden('id',$articulos[0]['id']);
-    }
-    // if(isset($check)){
-    //         echo    "<div>
-    //                  <span>accion completada</span>
-    //                 </div>";
-    //                 " <a href="echo base_url()/index.php> regresar</a>"
-    //     }
+        echo form_submit('enviar','Enviar',["class"=>"text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-5 py-2.5 block"]);
+        if(isset($articulos)){
+            echo form_hidden('id',$articulos[0]['id']);
+        }
+        // if(isset($check)){
+        //         echo    "<div>
+        //                  <span>accion completada</span>
+        //                 </div>";
+        //                 " <a href="echo base_url()/index.php> regresar</a>"
+        //     }
         echo form_close();
+        
     ?>
 </div>
